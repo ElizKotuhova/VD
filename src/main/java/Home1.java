b0058c50a28fb44e4774fd1fb0cebda7d99410e6
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2024-03-31 19:06:23 +0300

    Буратино и Барабас

---
+++ src/main/java/Home1.java
@@ -0,0 +1,10 @@
+public class Home1 {
+    public static void main(String[] args) {
+        final int buratino = 5;
+        System.out.println("buratino = " + buratino);
+        final int barabas = 3;
+        System.out.println("barabas = " + barabas);
+        final int leftovers = buratino - barabas;
+        System.out.println("leftovers = " + leftovers);
+    }
+}
