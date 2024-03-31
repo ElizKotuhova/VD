543664ed86a04f0be7a76b115e9cafcd88eabaab
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2024-03-31 19:25:26 +0300

    Саруман и Ланнистеры

--- src/main/java/Home2.java
+++ src/main/java/Home2.java
@@ -1,10 +1,10 @@
 public class Home2 {
     public static void main(String[] args) {
-        final int lanister = 10000;
-        System.out.println("Lanister: " + lanister);
+        final int lannister = 10000;
+        System.out.println("Lannister: " + lannister);
         final int saruman = 5000;
         System.out.println("Saruman: " + saruman);
-        final int total = lanister + saruman;
+        final int total = lannister + saruman;
         System.out.println("total: " + total + " infantry");
     }
 }
