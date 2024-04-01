766611170ada6af78bf4d6ff2467fd7f04037712
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2024-04-01 22:57:44 +0300

    Первоначальный взнос по ипотеке

---
+++ src/main/java/Home6.java
@@ -0,0 +1,11 @@
+public class Home6 {
+    public static void main(String[] args){
+        int flatPrice = 10_000_000;
+        System.out.println("flatPrice: " + flatPrice);
+        double percent = 20;
+        System.out.println("percent: " + percent);
+        double firstAmount = flatPrice / 100 * percent;
+        System.out.println("firstAmount: " + firstAmount);
+
+    }
+}
