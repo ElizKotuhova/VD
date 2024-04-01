591d4788ded475f0c0d8fdc9517a020ed3887c84
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2024-04-01 22:51:52 +0300

    Возраст Вселенной по сравнению с возрастом человечества

---
+++ src/main/java/Home5.java
@@ -0,0 +1,10 @@
+public class Home5 {
+    public static void main(String[] args){
+        long ageUniverse = 13_000_000_000L;
+        System.out.println("ageUniverse:" + ageUniverse);
+        int ageHuman = 100_000;
+        System.out.println("ageHuman:" + ageHuman);
+        long calculation = ageUniverse / ageHuman;
+        System.out.println("calculation:" + calculation);
+    }
+}
