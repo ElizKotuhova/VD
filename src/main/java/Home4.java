8cc81c86d6f66c35ee082ef938c4f95318b0f771
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2024-04-01 22:42:19 +0300

    Стакан, который наполовину полон

---
+++ src/main/java/Home4.java
@@ -0,0 +1,11 @@
+public class Home4 {
+    public static void main(String[] args){
+        int VolumeCup = 500;
+        System.out.println("VolumeCup: " + VolumeCup + "ml");
+        double percent = 50.0;
+        System.out.println("percent: " + percent);
+        double VolumeWater = VolumeCup / 100 * percent;
+        System.out.println("VolumeWater: " + VolumeWater);
+
+    }
+}
